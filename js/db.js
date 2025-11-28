2b628139524180156b72ecda150a319cf451cceb
FatinFityah <126367282+FatinFityah@users.noreply.github.com>
2025-11-28 12:56:13 +0800

    Create db.js

---
+++ js/db.js
@@ -0,0 +1,14 @@
+// js/db.js
+const products = [
+  // FOOD
+  { id: "f1", category: "food", name: "Nasi Ayam Crunchy", price: 12.00, img: "https://via.placeholder.com/300x200?text=Nasi+Ayam" },
+  { id: "f2", category: "food", name: "Nasi Lemak Kukus Biasa", price: 5.00, img: "https://via.placeholder.com/300x200?text=Nasi+Lemak" },
+  { id: "f7", category: "food", name: "Chicken Chop", price: 16.00, img: "https://via.placeholder.com/300x200?text=Chicken+Chop" },
+  
+  // BEVERAGES
+  { id: "b1", category: "beverage", name: "Sweet Strawberry Frappe", price: 8.00, img: "https://via.placeholder.com/300x200?text=Strawberry+Frappe" },
+  { id: "b2", category: "beverage", name: "Blue Monster Frappe", price: 9.00, img: "https://via.placeholder.com/300x200?text=Blue+Monster" },
+  
+  // DESSERT
+  { id: "d1", category: "dessert", name: "Strawberry Waffle", price: 6.00, img: "https://via.placeholder.com/300x200?text=Strawberry+Waffle" },
+];
